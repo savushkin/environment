0537b85c01c5adaed0cc9f52a49920a0c6ddc936
Ivan Savushkin <ivan@savushkin.me>
2020-01-26 21:23:42 +0300

    fix(builder): update kustomize script

--- docker-images/builder/scripts/kubectl.sh
+++ docker-images/builder/scripts/kubectl.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-mv ./kubectl /usr/local/bin
+EXECUTABLE_FULL_PATH="/usr/bin/kubectl"
+
+curl -s -L "https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VER}/bin/linux/amd64/kubectl" -o "$EXECUTABLE_FULL_PATH"
+chmod +x "$EXECUTABLE_FULL_PATH"
 
 mkdir -p ~/.kube
+
+kubectl version --client
--- docker-images/builder/scripts/kustomize.sh
+++ docker-images/builder/scripts/kustomize.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
 
-curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
-chmod +x ./kustomize
-mv ./kustomize /usr/local/bin
+EXECUTABLE_FULL_PATH="/usr/bin/kustomize"
+
+ARCHIVE="kustomize_v${KUSTOMIZE_VER}_linux_amd64.tar.gz"
+
+curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases | grep browser_download | grep "$ARCHIVE" | cut -d '"' -f 4 | sort | tail -n 1 | xargs curl -s -O -L
+
+tar xzf "$ARCHIVE"
+
+rm -f "$ARCHIVE"
+
+mv ./kustomize "${EXECUTABLE_FULL_PATH}"
+
+chmod +x "$EXECUTABLE_FULL_PATH"
+
+kustomize version
