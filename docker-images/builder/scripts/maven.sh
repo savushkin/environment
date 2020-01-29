3e1933d64b8fe0d149e7ad3b9dfa51e69aca6f1a
Ivan Savushkin <ivan@savushkin.me>
2020-01-29 14:41:22 +0300

    feat(builder): add maven package

---
+++ docker-images/builder/scripts/maven.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+BASE_URL=https://apache.osuosl.org/maven/maven-3/${MAVEN_VERSION}/binaries
+
+mkdir -p /usr/share/maven /usr/share/maven/ref
+curl -fsSL -o /tmp/apache-maven.tar.gz "${BASE_URL}/apache-maven-${MAVEN_VERSION}-bin.tar.gz"
+tar -xzf /tmp/apache-maven.tar.gz -C /usr/share/maven --strip-components=1
+rm -f /tmp/apache-maven.tar.gz
+ln -s /usr/share/maven/bin/mvn /usr/bin/mvn
+
+mvn -v
